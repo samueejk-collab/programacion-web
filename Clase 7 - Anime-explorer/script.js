80b97098a120458b23d8d1f20f144ce226cdaabd
Samuel <samueejk@gmail.com>
2026-06-17 11:25:19 -0300

    clase 7

--- Clase 7 - Anime-explorer/script.js
+++ Clase 7 - Anime-explorer/script.js
@@ -3,17 +3,60 @@ const peliculas = [
   {
     titulo: "Dead Poets Society",
     imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FuGa3NexFBtk12KT9vAoweVmN4k6.jpg&f=1&nofb=1&ipt=ca07b2ff892c5ce4faf62bd8f6d3f390bfb402d34f3fcae238fe68ea2b4684f6",
-    genero: "Tragedia",
+    genero: "Drama",
     rating: 9.0,
-    sinopsis: "se mueren todos"
+    sinopsis: ""
   },
-   {
+  {
     titulo: "Pulp Fiction",
-    imagen: "https://placehold.co/280x400/e94560/ffffff?text=Attack+on+Titan",
+    imagen: "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     genero: "Acción",
     rating: 9.0,
-    sinopsis: "lorem impsum"
+    sinopsis: ""
   },
+  {
+    titulo: "Perfect Days",
+    imagen: "https://m.media-amazon.com/images/M/MV5BNDdjNDBmM2EtMWVhNS00NTJjLWI2MmQtOTczZWNkOTdhNGIwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+  {
+    titulo: "Project Hail Mary ",
+    imagen: "https://multiplex.com.ar/wp-content/uploads/proyecto-fin-del-mundo.jpg",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+  {
+    titulo: "Look Back ",
+    imagen: "https://image.tmdb.org/t/p/original/1QJBTdtyH5aLrhjR3VbuxXNNnzH.jpg",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+  {
+    titulo: "Jujutsu Kaisen",
+    imagen: "https://m.media-amazon.com/images/I/818YClHyUjL._AC_UF894,1000_QL80_.jpg",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+   {
+    titulo: "Life is Beautiful",
+    imagen: "https://m.media-amazon.com/images/M/MV5BZTBhOGYzZjQtYzE0Mi00MGIwLWE0MWYtNzMxNTM2OTFkM2NjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+  {
+    titulo: "Aftersun",
+    imagen: "https://a.ltrbxd.com/resized/film-poster/8/6/8/5/5/8/868558-aftersun-0-1000-0-1500-crop.jpg?v=5ce2118fca",
+    genero: "Acción",
+    rating: 9.0,
+    sinopsis: ""
+  },
+  
 ];
 
 
@@ -77,4 +120,3 @@ document.querySelector("#genero").addEventListener("change", buscarYFiltrar);
 
 renderGrilla(peliculas);
 
-document.querySelector("#grilla").innerHTML = html;
